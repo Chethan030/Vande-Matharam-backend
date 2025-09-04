4e0765b74e8bb3b21fdbad8d05abb41e60e75b77
Chethan030 <chethan7234@gmail.com>
2025-09-04 15:37:27 +0530

    models_got_updated

--- GurukulamApp/models.py
+++ GurukulamApp/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Bgname(models.Model):
     name=models.CharField(max_length=30)
     def __str__(self):
@@ -19,4 +16,7 @@ class News(models.Model):
     image=models.ImageField(upload_to='news_images')
     description=models.TextField()
     date=models.DateField(auto_created=False)
+
+    def __str__(self):
+        return self.news_name
     
