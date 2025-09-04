739ac156678c005ed5bbddc8be99c130fae34c23
Vyshak N Shetty <vaishakvaishu285@gmail.com>
2025-09-04 15:14:38 +0530

    'serial_add'

--- GurukulamApp/views.py
+++ GurukulamApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import *
+
+class BgNameViews(ModelViewSet):
+    queryset=Bgname.objects.all()
+    serializer_class=BgNamesSerial
+
+class BgimagesUploadView(ModelViewSet):
+    queryset=BgImages.objects.all()
+    serializer_class=BgimagesSerial
